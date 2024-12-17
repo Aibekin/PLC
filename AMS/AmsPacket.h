3d3775fa1d769f854e03a0b4209a0e70d428316c
Aibekin <aibekin_zh_007@mail.ru>
2024-12-17 17:03:33 +0500

    new AMS/TCP Packet structure

--- AMS/AmsPacket.h
+++ AMS/AmsPacket.h
@@ -1,61 +1,68 @@
-#include <vector>
 #include <cstdint>
-#include <cstring> // Для memcpy
+#include <array>
+#include <vector>
+#include <cstring>
 #include <iostream>
 
-class AMSPacket
+// AMS/TCP header
+struct AmsTcpHeader
 {
-private:
+    uint32_t length;
+
+    AmsTcpHeader() : length(50) {}
+
+    void setLength(uint32_t len)
+    {
+        length = len;
+    }
+};
+
+// AMS header
+struct AmsHeader
+{
+    std::array<uint8_t, 6> targetNetId;
+    uint16_t targetPort;
+    std::array<uint8_t, 6> sourceNetId;
+    uint16_t sourcePort;
+    uint16_t commandId;
+    uint16_t stateFlags;
+    uint32_t dataLength;
+    uint32_t errorCode;
+    uint32_t invokeId;
+
+    AmsHeader()
+        : targetNetId({5, 114, 111, 44, 1, 1}), targetPort(801), sourceNetId({192, 168, 248, 165, 1, 1}), sourcePort(33013),
+          commandId(0x0002), stateFlags(0x0004), dataLength(12), errorCode(0), invokeId(0) {}
+};
+
+// AMS/TCP packet
+struct AMSPacket
+{
+    AmsTcpHeader tcpHeader;
+    AmsHeader amsHeader;
     uint32_t _indexGroup;
     uint32_t _indexOffset;
     uint32_t _length;
 
-public:
-    AMSPacket(uint32_t indexGroup, uint32_t indexOffset, uint32_t length) : _indexGroup(indexGroup), _indexOffset(indexOffset), _length(length) {}
+    AMSPacket(uint32_t indexGroup, uint32_t indexOffset, uint32_t length) : tcpHeader(), amsHeader(),
+                                                                            _indexGroup(indexGroup),
+                                                                            _indexOffset(indexOffset),
+                                                                            _length(length) {}
 
-    std::vector<uint8_t> create() // Read command packet
+    std::vector<uint8_t> create() const
     {
-        std::vector<uint8_t> request;
+        std::vector<uint8_t> buffer;
+        std::vector<uint32_t> data = {_indexGroup, _indexOffset, _length};
 
-        // AMS/TCP Header
-        uint8_t targetNetId[6] = {5, 114, 111, 44, 1, 1};
-        std::memcpy(&request[0], targetNetId, 6);
+        buffer.resize(sizeof(tcpHeader));
+        std::memcpy(buffer.data(), &tcpHeader, sizeof(tcpHeader));
 
-        uint16_t targetPort = htons(801);
-        std::memcpy(&request[6], &targetPort, 2);
+        size_t offset = buffer.size();
+        buffer.resize(offset + sizeof(amsHeader));
+        std::memcpy(buffer.data() + offset, &amsHeader, sizeof(amsHeader));
 
-        uint8_t sourceNetId[6] = {192, 168, 248, 165, 1, 1};
-        std::memcpy(&request[8], sourceNetId, 6);
+        buffer.insert(buffer.end(), data.begin(), data.end());
 
-        uint16_t sourcePort = htons(33013);
-        std::memcpy(&request[14], &sourcePort, 2);
-
-        uint16_t commandId = htons(0x0002); // 0x0002 - Read command
-        std::memcpy(&request[16], &commandId, 2);
-
-        uint16_t flagState = htons(0x0004); // ADS command flag
-        std::memcpy(&request[18], &flagState, 2);
-
-        uint32_t dataLength = htonl(12); // 4 indexGroup + 4 indexOffset + 4 length
-        std::memcpy(&request[20], &dataLength, 4);
-
-        uint32_t errorCode = htonl(0);
-        std::memcpy(&request[24], &errorCode, 4);
-
-        uint32_t invokeId = htonl(0);
-        std::memcpy(&request[28], &invokeId, 4);
-
-        // ADS Data
-
-        uint32_t indexGroup = htonl(_indexGroup);
-        std::memcpy(&request[32], &indexGroup, 4);
-
-        uint32_t indexOffset = htonl(_indexOffset);
-        std::memcpy(&request[36], &indexOffset, 4);
-
-        uint32_t length = htonl(_length);
-        std::memcpy(&request[40], &length, 4);
-
-        return request;
+        return buffer;
     }
 };
